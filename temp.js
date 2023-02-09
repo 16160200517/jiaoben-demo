e303773842e380a784633019ba8e71ff48a0959a
teller <940785602@qq.com>
2023-02-09 13:54:55 +0800

    add license

--- temp.js
+++ temp.js
@@ -5,6 +5,7 @@
 // @description  new script
 // @author       teller
 // @match        *://*
+// @license      AGPL License
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==
