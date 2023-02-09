18ff3c8bea9ef0c46945425bd26485636c780f74
teller <940785602@qq.com>
2023-02-09 13:48:10 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hello wrold
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  new script
+// @author       teller
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world')
+})();
